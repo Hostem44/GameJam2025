d556a9a16ff89cc24491422a21d6900274274e11
Cakey25 <cakey25yt@gmail.com>
2025-08-01 12:17:35 +0100

    dengeon createor makes new rooms and you can loop with the door, also accidentally made in the wrong branch

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public Vector2 TeleportDestination = new Vector2(0f, -4f);
+    public GameObject room;
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player") && Input.GetKey(KeyCode.E))
+        {
+            GameObject player = collision.gameObject;
+            DungeonController dungeon_con = room.transform.parent.gameObject.GetComponent<DungeonController>();
+            Room room_comp = room.GetComponent<Room>();
+            Debug.Log(room_comp);
+            int next_index = room_comp.dungeon_index;
+            next_index = (next_index + 1) % dungeon_con.LoopLength;
+
+            GameObject next_room = dungeon_con.dungeon[next_index];
+            Vector3 next_position = next_room.GetComponent<Room>().entry_point.position;
+            player.transform.position = next_position;
+        }
+    }
+    
 }
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public int dungeon_index;
+    public Transform entry_point;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
